810426657395bd338ab72e76bad67e996fb42b9d
oliverhuangchao <chaoh@g.clemson.edu>
2015-05-27 15:04:02 -0400

    change the skyline

--- heap/skyline.cpp
+++ heap/skyline.cpp
@@ -1,20 +1,21 @@
 typedef vector<int> vi;
 class mycomp{
 public:
-	bool operator()(const vector<int> &num1,const vector<int> &num2){
+bool operator()(const vector<int> &num1,const vector<int> &num2){
 		return num1.back() < num2.back();
 	}
 };
+void heapclear(priority_queue<vi,vector<vi>,mycomp> & heap){
+	while(!heap.empty()){
+		heap.pop();
+	}
+	return;
+}
 vector<pair<int,int>> getSkyline(vector<vector<int>> buildings){
-	priority_queue<vi,vector<vi>,mycomp> heap;
-	/*int x[] = {1,2,3};
-	int y[] = {2,3,4};
-	vector<int> num1 = getVector(x,3);
-	vector<int> num2 = getVector(y,3);
-	heap.push(num1);
-	heap.push(num2);
-	print(heap.top());*/
+	vector<pair<int,int>> getSkyline(vector<vector<int>> buildings){
 	vector<pair<int,int>> res;
+	if(buildings.empty()) return res;
+	priority_queue<vi,vector<vi>,mycomp> heap;
 	vector<int> tmp;
 	vector<int> top;
 	tmp = buildings[0];
@@ -25,13 +26,16 @@ vector<pair<int,int>> getSkyline(vector<vector<int>> buildings){
 		tmp = buildings[i];
 		top = heap.top();
 		if(tmp[0] > top[0]){
-			res.push(make_pair(top[1],0));
-			heap.clear();
+			res.push_back(make_pair(top[1],0));
+			heapclear(heap);
+			heap.push(tmp);
+			res.push_back(make_pair(tmp[0],tmp[2]));
+			i++;
 			continue;
 		}
 		if(tmp[2] > top[2]){
 			heap.push(tmp);
-			res.push(make_pair(tmp[0],tmp[2]));
+			res.push_back(make_pair(tmp[0],tmp[2]));
 		}
 		else{
 			if(tmp[1] > top[1]){
@@ -42,5 +46,17 @@ vector<pair<int,int>> getSkyline(vector<vector<int>> buildings){
 		}
 		i++;
 	}
+	while(!heap.empty()){
+		top = heap.top();
+		int w = top[1];
+		heap.pop();
+		if(!heap.empty()){
+		//	res.push_back(make_pair(w, 0));
+		//else{
+			top = heap.top();
+			res.push_back(make_pair(w, top[2]));
+		}
+	}
 	return res;
+}
 }
