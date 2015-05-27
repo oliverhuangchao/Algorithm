290ab57fcbe184adc9f80c05cc48a29834681faa
oliverhuangchao <chaoh@g.clemson.edu>
2015-05-27 12:03:36 -0400

    make comIII dfs easier

--- dfs/combinationsumIII.cpp
+++ dfs/combinationsumIII.cpp
@@ -1,18 +1,18 @@
 typedef vector<int> vi;
-void dfs(vi &path, vector<vi> &result, int current, int step, const int& k, int sum){
-    if(step == k && sum == 0){
+void dfs(vi &path, vector<vi> &result, const int& k, int sum){
+    if(path.size() == k && sum == 0){
         result.push_back(path);
         return;
     }
-    for(int i=current; i<10 && sum > 0; i++){
+    for(int i=path.empty()?1:path.back()+1; i<10 && sum > 0; i++){
         path.push_back(i);
-        dfs(path,result,i+1,step+1,k,sum-i);
-        path.pop_back(i);
+        dfs(path,result,k,sum-i);
+        path.pop_back();
     }
 }
 vector<vector<int>> combinationSum3(int k, int n) {
     vector<vi> res;
     vi path;
-    dfs(path,res,0,0,k,n);
+    dfs(path,res,k,n);
     return res;
 }
