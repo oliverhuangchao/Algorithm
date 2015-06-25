1a465c36a00a4eea580612595105f802c23a11f3
oliverhuangchao <chaoh@g.clemson.edu>
2015-06-25 10:29:53 -0700

    add string folder

---
+++ string/reverseWordII.cpp
@@ -0,0 +1,21 @@
+// given a sentense and reverse its words order
+// example:
+// input: hello world chaoh
+// output: chaoh world hello
+int main(){
+	//string x;
+	char a[128];
+	cin.getline(a,sizeof(a));
+	string str(a);
+	reverseword(str,0,str.size()-1);
+	int start = 0;
+	for(int i=0;i<str.size();i++){
+		if(str[i] == ' '){
+			reverseword(str,start,i-1);
+			start = i+1;
+		}
+	}
+	reverseword(str,start,str.size()-1);
+	cout<<str<<endl;
+	return 0;
+}
