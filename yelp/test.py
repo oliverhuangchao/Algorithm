17d2d9037e977fed92fb2db87665d2088ea0a8e4
oliverhuangchao <chaoh@g.clemson.edu>
2015-07-06 12:57:25 -0400

    first commit

--- yelp/test.py
+++ yelp/test.py
@@ -1,42 +1,15 @@
-import random
-class Balancer:
-    def __init__(self, weights):
-        self.weights = weights
-        self.total = sum([weight[1] for weight in weights])
-        start = 0
-        match_list = []
-        for weight in self.weights:
-            for i in range(0, weight[1]):
-                match_list.append(weight[0])
-        self.match_list = match_list       
-    def request(self):
-        balancer = random.randint(0,self.total-1)
-        return self.match_list[balancer]
+x = {"a":["p1","p2","p3"],"b":["p2","p4"],"c":["p1","p3"]}
+print x
+y = dict()
 
-# After writing this, I realized that my solution above is hacky as
-# heck; it works fine for the case where the sum is a small number and
-# the weights are all integers, but fails in any other case. This bugged
-# me enough to do something about it.
-class Balancer2:
-    def __init__(self, weights):
-        self.weights = weights
-        self.total = sum([weight[1] for weight in weights])
-        start = 0
-        match_list = []
-        cur = 0
-        for weight in self.weights:
-            match_list.append((cur, cur + weight[1], weight[0]))
-            cur += weight[1]
-        self.match_list = match_list       
-    def request(self):
-        balancer = random.random()*self.total
-        for item in self.match_list:
-            if balancer >= item[0] and balancer < item[1]:
-                return item[2]
-        print "What?!" 
+for i in x:
+    for j in x[i]:
+        #print j
+        if j in y:
+            y[j].append(i)
+        else:
+            y[j] = list()
+            y[j].append(i)
 
-
-w = [['a', 1], ['b', 1], ['c', 8]]
-print w
-res = Balancer2(w)
-print res.request()
+for i in y:
+    print i + str(y[i])
