858885bc2b6b7070b5536695214c915106d56f8c
oliverhuangchao <chaoh@g.clemson.edu>
2015-07-09 15:55:04 -0400

    add three functions

---
+++ epic/test.py
@@ -0,0 +1,6 @@
+#Telephone Number          
+
+#Print all valid phone numbers of length n subject to following constraints: 
+# If a number contains a 4, it should start with 4 
+# No two consecutive digits can be same 
+# Three digits (e.g. 7,2,9) will be entirely disallowed, take as input 
