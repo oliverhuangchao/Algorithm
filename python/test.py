4f14200022aafbfb9966b880c2c71537236481f6
oliverhuangchao <chaoh@g.clemson.edu>
2015-07-09 14:10:02 -0400

    add epic

--- python/test.py
+++ python/test.py
@@ -1,30 +1,36 @@
-# trap rain water problem
-# coding=utf-8
-import pdb
+# reverse list
 
-# @param heights: a list of integers
-# @return: a integer
-def trapRainWater(heights):
-    #对于任何一个坐标，检查其左右的最大坐标，然后相减就是容积
-    size = len(heights)
-    left = [heights[0] for i in range(size)]
-    right = [heights[-1] for i in range(size)]
-    max_value = 0
-    for i in range(size):
-        max_value = max(max_value,heights[i])
-        left[i] = max_value
-    max_value = heights[-1]
-    for i in range(2,size+1):
-        max_value = max(max_value,heights[size-i])
-        right[size-i] = max_value
-    res = 0
-    for i in range(size):
-        res += min(left[i],right[i])-heights[i]
+class listnode():
+    def __init__(self,x):
+        self._value = x
+        self._next = None
 
-    print heights
-    print left
-    print right
-    return res
+def revereList(head):
+    return rev(head,None)
 
-heights = [0,1,0,2,1,0,1,3,2,1,2,1]
-print trapRainWater(heights)
+
+def rev(head,newhead):
+    if head is None:
+        return newhead
+    else:
+        next = head._next
+        head._next = newhead
+        return rev(next,head)
+
+
+def printlist(head):
+    while head is not None:
+        print head._value
+        head = head._next
+
+
+x = map(lambda x:listnode(x),range(5))
+head = x[0]
+start = head
+for i in range(1,len(x)):
+    x[i-1]._next = x[i]
+    #i = head
+
+printlist(head)
+head = revereList(head)
+printlist(head)
